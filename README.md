d45644f1415239e9096bbf12f4e85f3c780df2a1
giang <muaha.vang02@gmail.com>
2025-02-28 21:34:40 +0700

    Revert "feat(auth): register..."
    This reverts commit 0982a982f02f9dfe55826600e773860c0b398f28.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # TOEIC-Learning-Api
-
-
--- src/server.js
+++ src/server.js
@@ -1,27 +1,12 @@
 require("dotenv").config();
-const authRoutes = require("./routes/AuthRoutes");
-const mongoose = require("mongoose");
 const express = require("express");
-const errorHandler = require("./middleware/errorHandle");
-
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("MongoDB Connected!");
-  } catch (error) {
-    console.error("MongoDB Connection Failed:", error);
-    process.exit(1);
-  }
-};
-
-connectDB()
-
-app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to TOEIC API");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
