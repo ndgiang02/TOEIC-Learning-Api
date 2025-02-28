1b0cda6e4a5a22194a27881b6e13d6e47b14be58
giang <muaha.vang02@gmail.com>
2025-02-28 21:44:49 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
+const authRoutes = require("./routes/AuthRoutes");
+const mongoose = require("mongoose");
 const express = require("express");
+const errorHandler = require("./middleware/errorHandle");
+
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to TOEIC API");
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+connectDB()
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
